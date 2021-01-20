cd23d16e5c53a91249942031b3e83b320df5e039
vijay <vijaymallela88@gmail.com>
2021-01-20 23:20:15 +0530

    indentation changes

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,18 +1,19 @@
 class Loan < ApplicationRecord
 
 	def self.loan_details(loan)
+		#(p x r x (1+r)**n)/((1+r)**(n-1))
 		loans = []
 		loan_release_date = loan.disbursement_date
 	  	loan_emi_start_date = loan_release_date
 	  	remaining_amount = loan.loan_amount.to_f
 	  	term = loan.term.to_i
 	  	interest = loan.rate_of_interest.to_f
-	  	total_loan_interest = (loan.loan_amount.to_f*(interest/100)*(1+(interest/100))**term)/((1+(interest/100))**(term-1))/2
-	  	emi_amount = (loan.loan_amount.to_f + total_loan_interest.to_i)/term
+	  	total_loan_interest = (loan.loan_amount.to_f * (interest / 100) * (1 + (interest / 100)) ** term) / ((1 + (interest / 100)) ** (term - 1)) / 2
+	  	emi_amount = (loan.loan_amount.to_f + total_loan_interest.to_i) / term
 	  	loan.term.times do
 	  		loan_arr = []
-	  	  interest_amount = ((remaining_amount.to_f/term)/100)*interest
-	  	  principal_amount = emi_amount - interest_amount
+	  	    interest_amount = ((remaining_amount.to_f / term) / 100) * interest
+	  	    principal_amount = emi_amount - interest_amount
 	  		loan_arr << loan_emi_start_date.next_month.beginning_of_month.strftime("%d/%m/%Y")
 	  		loan_arr << remaining_amount.round(2)
 	  		loan_arr << principal_amount.round(2)
